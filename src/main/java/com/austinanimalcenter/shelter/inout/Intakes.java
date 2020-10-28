53922026f216a40c7974372ed16aa1c7c5b3ae56
wehwan <we_hwan@naver.com>
2020-10-28 21:36:49 +0900

    add entity Intakes.

---
+++ src/main/java/com/austinanimalcenter/shelter/inout/Intakes.java
@@ -0,0 +1,36 @@
+package com.austinanimalcenter.shelter.inout;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+public class Intakes {
+
+    @Id
+    private String animalId;
+
+    private String ageUponIntake;
+
+    private String animalType;
+
+    private String breed;
+
+    private String color;
+
+    private Date datetime;
+
+    private Date datetime2;
+
+    private String foundLocation;
+
+    private String intakeCondition;
+
+    private String intakeType;
+
+    private String name;
+
+    private String sexUponIntake;
+
+}
