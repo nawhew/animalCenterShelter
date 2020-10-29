26c661c5925c88632b589ce48ecb81611f50ab22
nawhew <we_hwan@naver.com>
2020-10-29 12:51:17 +0900

    add entity Outcomes

---
+++ src/main/java/com/austinanimalcenter/shelter/inout/Outcomes.java
@@ -0,0 +1,34 @@
+package com.austinanimalcenter.shelter.inout;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Outcomes {
+
+    @Id
+    private String animalId;
+
+    private String ageUponOutcome;
+
+    private String animalType;
+
+    private String breed;
+
+    private String color;
+
+    private Date dateOfBirth;
+
+    private Date datetime;
+
+    private Date monthYear;
+
+    private String name;
+
+    private String outcomeSubtype;
+
+    private String outcomeType;
+
+    private String sexUponOutcome;
+}
