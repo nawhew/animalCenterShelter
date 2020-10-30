39a1560e770cd06eec81b2e3cf69bc61684c8409
wehwan <we_hwan@naver.com>
2020-10-30 22:46:27 +0900

    add entity InAndOut

---
+++ src/main/java/com/austinanimalcenter/shelter/inout/InAndOut.java
@@ -0,0 +1,26 @@
+package com.austinanimalcenter.shelter.inout;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity @Table(name = "INTAKE_OUTCOMES")
+public class InAndOut {
+
+    private String ageUponOutcome;
+
+    private String animalIdOutcome;
+
+    private Date dateOfBirth;
+
+    private String outcomeSubtype;
+
+    private String outcomeType;
+
+    private String sexUponOutcome;
+
+    private Float ageUponOutcomeDays;
+
+    private Float ageUponOutcomeYears;
+
+}
